0d7a44d0104eace5f6ebdaa9afe552beb4514bc0
abhishek0083 <146963477+abhishek0083@users.noreply.github.com>
2024-07-19 17:39:35 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,8 +3,9 @@ public class Solution{
 	public static void main(String[] args){
 		int a = 1;
 		int b = 2;
-		int c = a + b;
-		System.out.println(c);
+		int c = 3;
+		int d = a + b + c;
+		System.out.println(d);
 	}
 }
 
